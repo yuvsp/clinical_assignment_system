b1bfb92cf6964058f4c598b3988a2618536ab0ce
Yuval Shapira <yuvalsp@gmail.com>
2024-07-22 17:57:56 +0300

    db on /data

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+# config.py
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///clinical_assignment.db'
+    # Use an absolute path for the database file
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "data", "clinical_assignment.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24) 
- 
+    SECRET_KEY = os.urandom(24)
